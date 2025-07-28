622893a8ac5d114e38e6b81191b76c22b6a0698d
yklitnick <yitzchokyacovklitnick@gmail.com>
2025-07-28 19:43:31 -0400

    fix: add spaces in user join/exit messages for better readability

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 io.on('connection', function (socket) {
     socket.on('newuser', function (username) {
-        socket.broadcast.emit('update', username + 'joined the conversation');
+        socket.broadcast.emit('update', username + ' joined the conversation');
     });
     socket.on('exituser', function (username) {
-        socket.broadcast.emit('update', username + 'left the conversation');
+        socket.broadcast.emit('update', username + ' left the conversation');
     });
     socket.on('chat', function (message) {
         socket.broadcast.emit('chat', message);
